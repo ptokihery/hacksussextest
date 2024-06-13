f7edda619c33ecc1a0503faf5902c8819a46f9d7
tokihery <tokihery15@gmail.com>
2024-06-13 20:41:02 +0300

    fix: config test

--- wordpattern.js
+++ wordpattern.js
@@ -1,38 +1,7 @@
 import { exec } from "child_process"
 
 export function wordPattern(pattern, s) {
-    const words = s.split(' ');
 
-    if (pattern == "" &&  s == "") return true
-    if (pattern.length !== words.length) {
-        return false;
-    }
-
-    const patternToWord = new Map();
-    const wordToPattern = new Map();
-
-    for (let i = 0; i < pattern.length; i++) {
-        const p = pattern[i];
-        const w = words[i];
-
-        if (patternToWord.has(p)) {
-            if (patternToWord.get(p) !== w) {
-                return false;
-            }
-        } else {
-            patternToWord.set(p, w);
-        }
-
-        if (wordToPattern.has(w)) {
-            if (wordToPattern.get(w) !== p) {
-                return false;
-            }
-        } else {
-            wordToPattern.set(w, p);
-        }
-    }
-
-    return true;
 }
 
 // If this script is run directly, execute `npm test`
